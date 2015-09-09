ac69dc30fe49a4a28d1f60bc742536e971518bff
Amir Shavit <amir.shavit@autodesk.com>
2015-09-09 09:43:32 +0300

    Test strings that were altered

--- Examples/ADAppRater Demo/ADAppRater DemoTests/Model Object Tests/ADAppRaterTextsTests.m
+++ Examples/ADAppRater Demo/ADAppRater DemoTests/Model Object Tests/ADAppRaterTextsTests.m
@@ -27,28 +27,56 @@
 
 - (void)testAppRaterTextsAppName
 {
+    // Arrange
     NSString* appName = @"someName";
     ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
 
+    // Assert
     XCTAssertEqualObjects(textObject.applicationName, appName);
 }
 
 - (void)testAppRater_SatisfactionAlert_DefaultTexts
 {
+    // Arrange
     NSString* appName = @"someName";
     ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
     
+    // Assert
     XCTAssertEqualObjects(textObject.userSatisfactionAlertMessage, @"Do you find someName useful?");
     XCTAssertEqualObjects(textObject.userSatisfactionAlertAnswerYes, @"Yes");
     XCTAssertEqualObjects(textObject.userSatisfactionAlertAnswerNo, @"No");
     XCTAssertNil(textObject.userSatisfactionAlertTitle);
 }
 
-- (void)testAppRater_RatingAlert_DefaultTexts
+- (void)testAppRater_SatisfactionAlert_CustomTexts
 {
+    // Arrange
     NSString* appName = @"someName";
     ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
     
+    textObject.userSatisfactionAlertTitle = @"A title";
+    textObject.userSatisfactionAlertMessage = @"A message";
+    textObject.userSatisfactionAlertAnswerYes = @"A yes";
+    textObject.userSatisfactionAlertAnswerNo = @"A no";
+    
+    // Assert
+    XCTAssertNotNil(textObject.userSatisfactionAlertTitle,
+                    @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.userSatisfactionAlertMessage, @"Do you find someName useful?",
+                             @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.userSatisfactionAlertAnswerYes, @"Yes",
+                             @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.userSatisfactionAlertAnswerNo, @"No",
+                             @"This should not be the default string");
+}
+
+- (void)testAppRater_RatingAlert_DefaultTexts
+{
+    // Arrange
+    NSString* appName = @"someName";
+    ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
+    
+    // Assert
     XCTAssertEqualObjects(textObject.appRatingAlertTitle, @"Thank You");
     XCTAssertEqualObjects(textObject.appRatingAlertMessage, @"We're happy that you find someName useful! It'd be really helpful if you rated us.");
     XCTAssertEqualObjects(textObject.appRatingAlertAnswerRate, @"Rate someName");
@@ -56,77 +84,145 @@
     XCTAssertEqualObjects(textObject.appRatingAlertAnswerDontRate, @"No, Thanks");
 }
 
+- (void)testAppRater_RatingAlert_CustomTexts
+{
+    // Arrange
+    NSString* appName = @"someName";
+    ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
+
+    textObject.appRatingAlertTitle = @"A title";
+    textObject.appRatingAlertMessage = @"A message";
+    textObject.appRatingAlertAnswerRate = @"Button 1";
+    textObject.appRatingAlertAnswerRemindMe = @"Button 2";
+    textObject.appRatingAlertAnswerDontRate = @"Button 3";
+
+    // Assert
+    XCTAssertNotEqualObjects(textObject.appRatingAlertTitle, @"Thank You",
+                             @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.appRatingAlertMessage, @"We're happy that you find someName useful! It'd be really helpful if you rated us.",
+                             @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.appRatingAlertAnswerRate, @"Rate someName",
+                             @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.appRatingAlertAnswerRemindMe, @"Remind Me Later",
+                             @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.appRatingAlertAnswerDontRate, @"No, Thanks",
+                          @"This should not be the default string");
+}
+
 - (void)testAppRater_FeedbackAlert_DefaultTexts
 {
+    // Arrange
     NSString* appName = @"someName";
     ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
     
+    // Assert
     XCTAssertNil(textObject.userFeedbackAlertTitle);
     XCTAssertEqualObjects(textObject.userFeedbackAlertMessage, @"Please let us know how to make someName better for you!");
     XCTAssertEqualObjects(textObject.userFeedbackAlertAnswerYes, @"Contact us");
     XCTAssertEqualObjects(textObject.userFeedbackAlertAnswerNo, @"No thanks");
 }
 
+- (void)testAppRater_FeedbackAlert_CustomTexts
+{
+    // Arrange
+    NSString* appName = @"someName";
+    ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
+
+    textObject.userFeedbackAlertTitle = @"A title";
+    textObject.userFeedbackAlertMessage = @"A message";
+    textObject.userFeedbackAlertAnswerYes = @"A yes";
+    textObject.userFeedbackAlertAnswerNo = @"A no";
+
+    // Assert
+    XCTAssertNotNil(textObject.userFeedbackAlertTitle,
+                 @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.userFeedbackAlertMessage, @"Please let us know how to make someName better for you!",
+                          @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.userFeedbackAlertAnswerYes, @"Contact us",
+                          @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.userFeedbackAlertAnswerNo, @"No thanks",
+                          @"This should not be the default string");
+}
+
 - (void)testAppRater_ThankUserAlert_DefaultTexts
 {
+    // Arrange
     NSString* appName = @"someName";
     ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
     
+    // Assert
     XCTAssertEqualObjects(textObject.thankUserAlertTitle, @"Thanks!");
     XCTAssertEqualObjects(textObject.thankUserAlertMessage, @"Thank you for your feedback!");
     XCTAssertEqualObjects(textObject.thankUserAlertDismiss, @"Close");
 }
 
+- (void)testAppRater_ThankUserAlert_CustomTexts
+{
+    // Arrange
+    NSString* appName = @"someName";
+    ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
+
+    textObject.thankUserAlertTitle = @"Title";
+    textObject.thankUserAlertMessage = @"Message";
+    textObject.thankUserAlertDismiss = @"Button";
+
+    // Assert
+    XCTAssertNotEqualObjects(textObject.thankUserAlertTitle, @"Thanks!",
+                          @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.thankUserAlertMessage, @"Thank you for your feedback!",
+                          @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.thankUserAlertDismiss, @"Close",
+                          @"This should not be the default string");
+}
+
 - (void)testAppRater_FeedbackForm_DefaultTexts
 {
+    // Arrange
     NSString* appName = @"someName";
     ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
     
+    // Assert
     XCTAssertEqualObjects(textObject.feedbackFormRecipient, @"someMail");
     XCTAssertEqualObjects(textObject.feedbackFormSubject, @"Feedback");
     XCTAssertNil(textObject.feedbackFormBody);
 }
 
+- (void)testAppRater_FeedbackForm_CustomTexts
+{
+    // Arrange
+    NSString* appName = @"someName";
+    ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName feedbackRecipientEmail:@"someMail"];
+
+    textObject.feedbackFormRecipient = @"other email";
+    textObject.feedbackFormSubject = @"subject";
+    textObject.feedbackFormBody = @"body";
+
+    // Assert
+    XCTAssertNotEqualObjects(textObject.feedbackFormRecipient, @"someMail",
+                          @"This should not be the default string");
+    XCTAssertNotEqualObjects(textObject.feedbackFormSubject, @"Feedback",
+                          @"This should not be the default string");
+    XCTAssertNotNil(textObject.feedbackFormBody,
+                 @"This should not be the default string");
+}
+
 - (void)testAppRater_FeedbackForm_DefaultTexts_initWithoutEmail
 {
+    // Arrange
     NSString* appName = @"someName";
     ADAppRaterTexts* textObject = [[ADAppRaterTexts alloc] initWithApplicationName:appName];
     
     id mockAppRater = OCMStrictClassMock([ADAppRater class]);
     OCMExpect([mockAppRater AR_logConsole:@"WARNING!! No email address provided for feedback form recipient!!"]);
-    
+
+    // Act
     NSString* getMail = textObject.feedbackFormRecipient;
     
+    // Assert
     XCTAssertNil(getMail);
     XCTAssertEqualObjects(textObject.feedbackFormSubject, @"Feedback");
     XCTAssertNil(textObject.feedbackFormBody);
     OCMVerifyAll(mockAppRater);
 }
 
-/**
-@property (nonatomic, strong) NSString* userSatisfactionAlertTitle;
-@property (nonatomic, strong) NSString* userSatisfactionAlertMessage;
-@property (nonatomic, strong) NSString* userSatisfactionAlertAnswerYes;
-@property (nonatomic, strong) NSString* userSatisfactionAlertAnswerNo;
-
-@property (nonatomic, strong) NSString* appRatingAlertTitle;
-@property (nonatomic, strong) NSString* appRatingAlertMessage;
-@property (nonatomic, strong) NSString* appRatingAlertAnswerRate;
-@property (nonatomic, strong) NSString* appRatingAlertAnswerRemindMe;
-@property (nonatomic, strong) NSString* appRatingAlertAnswerDontRate;
-
-@property (nonatomic, strong) NSString* userFeedbackAlertTitle;
-@property (nonatomic, strong) NSString* userFeedbackAlertMessage;
-@property (nonatomic, strong) NSString* userFeedbackAlertAnswerYes;
-@property (nonatomic, strong) NSString* userFeedbackAlertAnswerNo;
-
-@property (nonatomic, strong) NSString* thankUserAlertTitle;
-@property (nonatomic, strong) NSString* thankUserAlertMessage;
-@property (nonatomic, strong) NSString* thankUserAlertDismiss;
-
-@property (nonatomic, strong) NSString* feedbackFormRecipient;
-@property (nonatomic, strong) NSString* feedbackFormSubject;
-@property (nonatomic, strong) NSString* feedbackFormBody;
-8
-*/
 @end
